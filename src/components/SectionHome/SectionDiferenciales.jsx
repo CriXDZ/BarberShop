66f15bf950418aad7f3afce3c4cb3d0428758e4f
CriXDZ <dj20000711@gmail.com>
2025-03-06 00:56:49 -0500

    removed video section

--- src/components/SectionHome/SectionDiferenciales.jsx
+++ src/components/SectionHome/SectionDiferenciales.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import diferenciales from "../../assets/diferenciales/diferenciales.jpg";
-import "@justinribeiro/lite-youtube";
 
 const SectionDiferenciales = () => {
   return (
@@ -8,34 +7,18 @@ const SectionDiferenciales = () => {
       <h3 className="titulo-principal">Diferenciales</h3>
       <div className="contenido-diferenciales">
         <ul className="lista-diferenciales">
-          <li className="items">Atención personalizada a los clientes</li>
-          <li className="items">Espacio diferenciado</li>
-          <li className="items">Localización</li>
-          <li className="items">Profesionales calificados</li>
-          <li className="items">Puntualidad</li>
-          <li className="items">Limpieza</li>
+          <li>Atención personalizada a los clientes</li>
+          <li>Espacio diferenciado</li>
+          <li>Localización</li>
+          <li>Profesionales calificados</li>
+          <li>Puntualidad</li>
+          <li>Limpieza</li>
         </ul>
         <img
           src={diferenciales}
           alt="Diferenciales de Barbería Alura"
           className="imagen-diferenciales"
         />
-      </div>
-
-      <div>
-        <h3 className="titulo-principal">multimedia</h3>
-        <p className="texto-video">
-          Video que destaca la habilidad y dedicación de nuestros barberos,
-          reflejando la calidad y el estilo único de nuestros servicios.
-        </p>
-      </div>
-
-      <div className="video-container">
-        <lite-youtube
-          videoid="wcVVXUV0YUY"
-          title="Video de YouTube"
-          className="video"
-        ></lite-youtube>
       </div>
     </section>
   );
